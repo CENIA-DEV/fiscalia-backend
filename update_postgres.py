e9cb239877487dc6f2277ae71c534fd07dadd824
Tomás Gómez <ftgomez@uc.cl>
2024-08-02 16:48:02 -0400

    change base path update_postgres

--- update_postgres.py
+++ update_postgres.py
@@ -1,9 +1,7 @@
-import os
-
 from src.postgres import PostgresDB
 from src.queries import OracleDB
 
-BASE_PATH = f"{os.getcwd()}/fiscalia-backend/src/dicts/"
+BASE_PATH = "./fiscalia-backend/src/dicts/"
 
 
 def update_rucs(oracle, postgres):
